bc7ae18715c7436b52b767329d770fca54bd4329
Redoan23 <redoan1080@gmail.com>
2024-04-26 00:59:30 +0600

    added Authcontext

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
-
+import React, { useContext } from 'react';
+import { AuthContext } from '../../AuthProvider/AuthProvider';
 const Home = () => {
+    const { name } = useContext(AuthContext)
     return (
         <div>
-            <h1>this is home of the website</h1>
+            <h1>this is home of the website and name is {name }</h1>
         </div>
     );
 };
